dfe98476e637fc3219ea5605385f0f2f872eb22f
Rohan Nobis <nobisro@gmail.com>
2018-12-07 19:01:32 -0500

    handling post requests successfully

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const watson = require("../helpers/watson");
+const bodyParser = require("body-parser");
 
 const port = 8080;
 
 app.use(express.static("../public"));
+app.use(bodyParser.json());
 
 app.get("/data", (req, res) => {
   console.log("req received!");
@@ -13,8 +15,9 @@ app.get("/data", (req, res) => {
   });
 });
 
-app.post("/data", (req, res) => {
-  console.log(req.body);
+app.post("/api/blob", (req, res) => {
+  console.log(req.body.data);
+  console.log(req.body.data);
   res.send(200);
 });
 
