8e24ee0a363ade0fc248425f6007943859e323f7
Rohan Nobis <nobisro@gmail.com>
2018-12-09 17:19:42 -0500

    rendering out Topic and original Microhpone components

--- client/App.jsx
+++ client/App.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 import Microphone from "./Microphone.jsx";
 // import handleAudioSuccess from "../helpers/handleAudioSuccess";
 import InteractiveMic from "./InteractiveMic.jsx";
+import Topic from "./Topic.jsx";
 
 class App extends React.Component {
   constructor(props) {
@@ -29,15 +30,21 @@ class App extends React.Component {
     return (
       <React.Fragment>
         <button onClick={this.convertAudioFile}>
-          Click to send audio to Watson!
+          Click to send audio to Watson!!
         </button>
         <div>{transcript}</div>
 
-        <InteractiveMic sendBlob={this.sendBlob} />
+        <Microphone />
+        <section className="mw7 center">
+          <h2 className="athelas ph3 ph0-l">Speaking Practice</h2>
+          <Topic />
+        </section>
       </React.Fragment>
     );
   }
 }
+
+//  <InteractiveMic sendBlob={this.sendBlob} />
 
 export default App;
 
