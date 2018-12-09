a06491f9550c4faa82d8236f4ffe4649e189ce7f
Rohan Nobis <nobisro@gmail.com>
2018-12-09 17:20:22 -0500

    added multer middlewear to save blob upload on post requests

--- server/index.js
+++ server/index.js
@@ -1,12 +1,22 @@
 const express = require("express");
+const multer = require("multer");
+const storage = multer.memoryStorage();
+const upload = multer({ dest: __dirname + "/uploads/" });
+const type = upload.single("upl");
 const app = express();
 const watson = require("../helpers/watson");
 const bodyParser = require("body-parser");
+const convertToOgg = require("../helpers/convertToOgg");
 
 const port = 8080;
 
 app.use(express.static("../public"));
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: true
+//   })
+// );
 
 app.get("/data", (req, res) => {
   console.log("req received!");
@@ -15,10 +25,13 @@ app.get("/data", (req, res) => {
   });
 });
 
-app.post("/api/blob", (req, res) => {
-  console.log(req.body.data);
-  console.log(req.body.data);
-  res.send(200);
+app.post("/api/blob", type, (req, res) => {
+  console.log("post req hit!");
+  console.log("req.body", req.body);
+  console.log("req.file:", req.file);
+  convertToOgg(req.file);
+
+  res.sendStatus(200);
 });
 
 app.listen(port, () => {
