3de605e295c5d28699ccb7f6b7a80a7a41722571
Rohan Nobis <nobisro@gmail.com>
2018-12-09 17:21:24 -0500

    incomplete, but able to receive blob and write encoded data to file

--- helpers/convertToOgg.js
+++ helpers/convertToOgg.js
@@ -1,5 +1,36 @@
 const fs = require("fs");
+const atob = require("atob");
 
-function convertToOgg(blobString) {}
+function convertToOgg(file) {
+  //   console.log("blob: ", JSON.parse(blobString.blob));
+  let filename = file.filename;
+  let path = file.path;
+  console.log("filename:", filename);
+
+  fs.writeFileSync("new_recording.ogg", path, "utf8", err => {
+    if (err) {
+      console.log("there was an error....");
+    } else {
+      console.log("file written");
+    }
+  });
+
+  //   const buff = Buffer.from(data);
+
+  //   ArrayBuffer;
+
+  //   console.log("buf:", data);
+  //   var fileReader = new FileReader();
+  //   console.log("line 8");
+  //   fileReader.onload = () => {
+  //     fs.writeFileSync("recording.ogg", Buffer.from(blob2));
+  //   };
+
+  //   fileReader.readAsArrayBuffer(blob);
+
+  //   const path = `${__dirname}/../assets/record.ogg`;
+
+  //   fs.writeFileSync(path, buff);
+}
 
 module.exports = convertToOgg;
