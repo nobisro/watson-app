a0e24126373468966f891041662d84ea1bd6613e
Rohan Nobis <nobisro@gmail.com>
2018-12-11 01:19:24 -0500

    changed content mime type to reflect ogg audio blob stream

--- helpers/watson.js
+++ helpers/watson.js
@@ -2,14 +2,18 @@ const SpeechToTextV1 = require("watson-developer-cloud/speech-to-text/v1");
 const API = require("../API.js");
 const fs = require("fs");
 
-function asyncWatson() {
+function asyncWatson(path) {
   var speechToText = new SpeechToTextV1({
     iam_apikey: API.KEY,
     url: API.URL
   });
 
-  const params = {
-    audio: fs.createReadStream("../assets/audio-file.flac")
+  // const buff = Buffer.from(path);
+  // const audioData = fs.readFileSync(buff);
+  //fs.createReadStream(`${path}`)
+  let params = {
+    audio: fs.createReadStream(`${path}`),
+    content_type: "audio/webm"
   };
 
   return new Promise((resolve, reject) => {
